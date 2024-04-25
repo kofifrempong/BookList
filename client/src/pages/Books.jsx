a24b74ae7165cd144ba57fda7e7bdeb20d0f0a82
Kofi Frempong <kofifrempong1@gmail.com>
2024-04-25 17:32:20 -0600

    commit msg

---
+++ client/src/pages/Books.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+
+import { Link } from "react-router-dom";
+
+const Books = () => {
+    const [books,setBooks] = useState([])
+
+    useEffect ( ()=>{
+        const fetchAllBooks = async () => {
+            try {
+                const res = await axios.get("http://localhost:8800/books")
+                setBooks(res.data);
+                console.log(res);
+
+            } catch(err){
+
+                console.log(err);
+            }
+        }
+        fetchAllBooks()
+
+
+    }, [])
+
+    const handleDelete = async (id)=> {
+        try {
+            await axios.delete("http://localhost:8800/books/" + id)
+            window.location.reload()
+
+        } catch(err){
+
+            console.log(err);
+        }
+    }
+        
+
+    return (
+        <div>
+            <h1>Book Shop</h1>
+            <div className="books">
+                {books.map((book) => (
+                    <div className="book" key ={book.id}>
+                        {book.cover && <img src = {book.cover} alt="" />}
+                        <h2> {book.title}</h2>
+                        <p>{book.desc}</p>
+                        <span>{book.price}</span>
+                        <button className='delete' onClick= {()=>handleDelete(book.id)}>Delete</button>
+                        <button className='update'><Link to={`/update/${book.id}`}>Update</Link></button>
+
+
+                    </div>
+                ))}
+
+            </div>
+            <button>
+                <Link to="/add">Add new book</Link>
+            </button>
+        </div>
+    )
+}
+
+export default Books
